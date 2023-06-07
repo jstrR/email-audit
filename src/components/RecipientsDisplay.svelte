f8d6dc8e5402313321e32df2f9b2795a2245d345
Sergey Rovkach <srovk22@gmail.com>
2023-06-07 15:35:27 +0900

    feat: refactor recipientsDisplay

--- src/components/RecipientsDisplay.svelte
+++ src/components/RecipientsDisplay.svelte
@@ -1,50 +1,42 @@
 <script lang="ts">
   import { onMount, onDestroy } from 'svelte'
-
   export let recipients: string[]
-
   let recipientsColumn: HTMLElement
   let visibleCount = 1
   let hiddenCount = 0
-
   const ellipsisWidth = 16
-
   onMount(() => {
     handleResize()
+    // comment line below if updating counter of hidden emails on resize is not needed
     window.addEventListener('resize', handleResize)
   })
-
   onDestroy(() => {
+    // comment line below if updating counter of hidden emails on resize is not needed
     window.removeEventListener('resize', handleResize)
   })
-
   function handleResize() {
-    if (recipientsColumn) {
-      const recipientsListItems: NodeListOf<HTMLElement> = recipientsColumn.querySelectorAll(
-        '.mail-item'
-      )
-      let recipientsColumnWidth = recipientsColumn.offsetWidth
-
-      visibleCount = 1
-      hiddenCount = 0
-
-      if (recipientsListItems.length > 1) {
-        recipientsListItems.forEach((recipientListItem, index) => {
-          const recipientItemWidth = recipientListItem.offsetWidth
-
-          if (
-            recipientItemWidth !== recipientsColumnWidth &&
-            recipientItemWidth + ellipsisWidth > recipientsColumnWidth &&
-            index !== 0
-          ) {
-            hiddenCount++
-          } else {
-            index !== 0 && visibleCount++
-          }
-          recipientsColumnWidth -= recipientItemWidth
-        })
-      }
+    if (!recipientsColumn || recipientsColumn.childElementCount <= 1) {
+      return
     }
+    const recipientsListItems = recipientsColumn.childNodes as NodeListOf<
+      HTMLElement
+    >
+    let recipientsColumnWidth = recipientsColumn.offsetWidth
+    visibleCount = 1
+    hiddenCount = 0
+    recipientsListItems.forEach((recipientListItem, index) => {
+      const recipientItemWidth = recipientListItem.offsetWidth
+      if (
+        recipientItemWidth !== recipientsColumnWidth &&
+        recipientItemWidth + ellipsisWidth > recipientsColumnWidth &&
+        index !== 0
+      ) {
+        hiddenCount++
+      } else {
+        index !== 0 && visibleCount++
+      }
+      recipientsColumnWidth -= recipientItemWidth
+    })
   }
 </script>
 
@@ -56,7 +48,7 @@
     height: 100%;
     white-space: nowrap;
   }
-  .item-wrapper {
+  .recipients-wrapper {
     margin-right: 0.125rem;
     overflow-x: hidden;
     text-overflow: ellipsis;
@@ -75,7 +67,7 @@
     border-radius: 3px;
     color: $color-secondary;
     display: flex;
-    padding: 0 0.5rem;
+    padding: 2px 5px;
     height: 100%;
   }
 </style>
@@ -83,18 +75,18 @@
 <!--
 @component
 Receives a `recipients` prop and renders all recepients
-
 - Usage:
   ```tsx
   <RecipientsDisplay {recipients} />
   ```
 -->
 <div class="wrapper">
-  <div class="item-wrapper" bind:this={recipientsColumn}>
+  <div class="recipients-wrapper" bind:this={recipientsColumn}>
     {#each recipients as recipient, index}
       <span
         class={`mail-item ${visibleCount === 1 && index === 0 ? 'mail-item-initial' : ''}`}>
-        {recipient}{#if recipients.length - 1 !== index},&nbsp;{/if}
+        {recipient}
+        {#if recipients.length - 1 !== index},&nbsp;{/if}
       </span>
     {/each}
   </div>
